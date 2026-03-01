559588919e9ead521a16f62d57f2155698775398
Harol <harolav3@gmail.com>
2026-03-01 17:51:12 -0500

    feat: remove unnecesary code

--- nvim/lua/global/general.lua
+++ nvim/lua/global/general.lua
@@ -17,15 +17,3 @@ vim.keymap.set('x', 'p', function()
   return 'pgv"' .. vim.v.register .. 'y`>'
 end, { expr = true })
 -- now it is possible to paste many times over selected text
-
--- show end blank line -- before it. nvim hidden end blank line (for example this blank line is used by PEP 8 in python files)
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
-  callback = function()
-    if vim.opt_local.endofline:get() then
-      vim.opt_local.endofline = false
-      vim.opt_local.fixendofline = false
-      vim.api.nvim_buf_set_lines(0, -1, -1, false, {""})
-    end
-  end,
-})
